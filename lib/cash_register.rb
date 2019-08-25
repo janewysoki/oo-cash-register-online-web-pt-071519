db7556db332b5013432283c8105b3201e94cfba4
Jane Wysoki <jane.wysoki@gmail.com>
2019-08-25 21:05:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,12 +3,12 @@ class CashRegister
   attr_accessor :discount, :total
   
   def initialize(discount=nil) 
-    @total = 0.0
+    @total = 0 
     @discount = discount
   end
   
   def add_item(title, price, quantity=nil)
-    @total = total + price * quantity  #@total += price is the same as saying @total = total + price
+    @total += price * quantity  #@total += price is the same as saying @total = total + price
   end
 
 end
