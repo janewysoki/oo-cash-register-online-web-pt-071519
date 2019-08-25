2245e940759325e5bc5c465a040b601575f7c8b0
Jane Wysoki <jane.wysoki@gmail.com>
2019-08-25 22:33:04 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,9 @@ class CashRegister
   end
   
   def add_item(title, price, quantity=1)
-    @items << title
+    quantity.times do 
+      @items << title
+    end
     @total += price * quantity  #@total += price is the same as saying @total = total + price
    end
 
